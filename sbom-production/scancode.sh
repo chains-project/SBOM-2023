cd036f969ff8931cab75b07f9d3ca130baebf569
Martin <wittlinger.martin@gmail.com>
2023-05-05 13:19:21 +0200

    🐛 Fix docker call in scancode.sh
    This commit fixes a docker call in scancode.sh that was generating an incorrect output. The --json-pp flag was replaced by --cyclonedx for generating the result file.

--- sbom-production/scancode.sh
+++ sbom-production/scancode.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Build the scancode docker image
 git clone https://github.com/nexB/scancode-toolkit
+git checkout 52138807d064e270ad4cf5e606f4fd6f266853fa
 cd scancode-toolkit && docker build -t scancode .
 rm -r scancode-toolkit
 
@@ -12,7 +13,7 @@ for f in ./study-subjects-env/*; do
   echo "Running scancode for $filename"
   cat ./study-subjects-env/$filename.env | grep "RepoUrl" | cut -d "=" -f2 | xargs git clone
   # Run the scancode docker image with the environment variables from ./study-subjects-env/$filename.env
-  container_id="$(docker run  -d -v $(pwd)/$filename:/$filename scancode -clpieu --json-pp $filename/bom.scancode.json /$filename)"
+  container_id="$(docker run  -d -v $(pwd)/$filename:/$filename scancode -clpieu --cyclonedx $filename/bom.scancode.json /$filename)"
   # docker wait $container_id
   rm -r $filename
   mkdir -p ./results/$filename/scancode
